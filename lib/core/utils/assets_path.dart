917871c047837b0e94255d37bcb181b3c41c0212
Yesra Fatima <147398141+yesra29@users.noreply.github.com>
2026-01-20 02:00:25 +0530

    assets path refracted

--- lib/core/utils/assets_path.dart
+++ lib/core/utils/assets_path.dart
@@ -1,7 +1,7 @@
 class AssetsPath {
-  static const String home = 'packages/flutter_kanpur_ui_kit/assets/home.svg';
-  static const String search = 'packages/flutter_kanpur_ui_kit/assets/search.svg';
-  static const String create = 'packages/flutter_kanpur_ui_kit/assets/create.svg';
-  static const String trending = 'packages/flutter_kanpur_ui_kit/assets/trending.svg';
-  static const String profile = 'packages/flutter_kanpur_ui_kit/assets/profile.svg';
+  static const String home = 'assets/home.svg';
+  static const String search = 'assets/search.svg';
+  static const String create = 'assets/create.svg';
+  static const String trending = 'assets/trending.svg';
+  static const String profile = 'assets/profile.svg';
 }
